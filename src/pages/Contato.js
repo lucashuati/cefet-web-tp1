0c2e6861f709ba3e9490f1323239721d89050b6b
Lucas Corrêa <lucashuati@hotmail.com>
2018-09-23 12:08:37 -0300

    Working on Contato page

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Row, Col } from 'react-materialize';
 
 class Contato extends React.Component {
   render() {
     return (
-      <h2>Contato</h2>
+      <Row>
+        <Col s={12}>
+          <h1>Contato</h1>
+        </Col>
+        <Col s={6}>
+          <h4>Fale comigo</h4>
+        </Col>
+        <Col s={6}>
+          <h4>Venha me encontrar</h4>
+        </Col>
+      </Row>
     );
   }
 }
