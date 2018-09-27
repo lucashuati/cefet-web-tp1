6902fcb90b874186600f0222717e4c622da3ec89
Lucas Corrêa <lucashuati@hotmail.com>
2018-09-27 11:31:21 -0300

    Add social media to contato page

--- src/components/FaleComigo.js
+++ src/components/FaleComigo.js
@@ -8,14 +8,34 @@ class FaleComigo extends React.Component {
         {
           label: 'Nome',
           value: 'Lucas Nascimento Huati Corrêa',
+          key: 'nome',
         },
         {
           label: 'Telefone',
           value: '(31) 9 9999 - 9999',
+          key: 'tel',
         },
         {
           label: 'Email',
           value: 'lucashuati@gmail.com',
+          key: 'mail',
+        },
+      ],
+      socialMedias: [
+        {
+          link: 'github.com',
+          logo: <i className="fab fa-github" />,
+          key: 'github',
+        },
+        {
+          link: 'linkedin.com',
+          logo: <i className="fab fa-linkedin-in" />,
+          key: 'linkedin',
+        },
+        {
+          link: 'twitter.com',
+          logo: <i className="fab fa-twitter" />,
+          key: 'twitter',
         },
       ],
     };
@@ -24,9 +44,18 @@ class FaleComigo extends React.Component {
   renderInfos() {
     const { infos } = this.state;
     return infos.map(info => (
-      <p>
+      <p key={info.key}>
         <span>{info.label}:</span> {info.value}
       </p>
+    ));
+  }
+
+  renderSocialMedias() {
+    const { socialMedias } = this.state;
+    return socialMedias.map(socialMedia => (
+      <div key={socialMedia.key} className="col m3">
+        <a href={socialMedia.link}>{socialMedia.logo}</a>
+      </div>
     ));
   }
 
@@ -35,6 +64,8 @@ class FaleComigo extends React.Component {
       <React.Fragment>
         <h4>Fale comigo</h4>
         {this.renderInfos()}
+        <h5>Redes Sociais</h5>
+        {this.renderSocialMedias()}
       </React.Fragment>
     );
   }
